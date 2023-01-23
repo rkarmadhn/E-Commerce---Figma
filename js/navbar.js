342bb57724adba3b2a8bcf96c35e17b7037a45f1
rkarmadhn <rakaaputtra1@gmail.com>
2023-01-23 18:50:51 +0700

    Fix Button Search

--- js/navbar.js
+++ js/navbar.js
@@ -6,12 +6,14 @@ btnShow.onclick = () => {
   btnShow.style.display = "none";
   btnHide.style.display = "block";
   navItems.style.left = "0";
+  inputSearch.classList.add('hide')
 };
 
 btnHide.onclick = () => {
   btnShow.style.display = "block";
   btnHide.style.display = "none";
   navItems.style.left = "-100%";
+  inputSearch.classList.add('hide')
 };
 
 // dropdown
@@ -21,18 +23,17 @@ const arrowUp = document.querySelector(".fa-arrow-up");
 const arrowDown = document.querySelector(".fa-arrow-down");
 
 dropdownTitle.onclick = () => {
-  dropdownMenu.classList.toggle("show");
-  dropdownMenu.classList.toggle("hide");
-  arrowUp.classList.toggle("show");
-  arrowUp.classList.toggle("hide");
-  arrowDown.classList.toggle("show");
-  arrowDown.classList.toggle("hide");
+    dropdownMenu.classList.toggle("show");
+    dropdownMenu.classList.toggle("hide");
+    arrowUp.classList.toggle("show");
+    arrowUp.classList.toggle("hide");
+    arrowDown.classList.toggle("show");
+    arrowDown.classList.toggle("hide");
 };
 
 // form
 const btnSearch = document.querySelector(".fa-magnifying-glass");
 const inputSearch = document.querySelector(".input-search");
-
 btnSearch.onclick = () => {
   inputSearch.classList.toggle("hide");
   inputSearch.classList.toggle("show");
